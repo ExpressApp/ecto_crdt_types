05326f80fa5599425b7e4577e59ad32b7ccfdfda
Alexander Malaev <am@malaev.net>
2017-06-01 17:17:18 +0300

    Bump version && fix configs for prod/dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EctoCrdtTypes.Mixfile do
 
   def project do
     [app: :ecto_crdt_types,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
