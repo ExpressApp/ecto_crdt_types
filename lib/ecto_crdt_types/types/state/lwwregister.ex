23fc366f0fc9040df03185d5fe9cdf0fe745a8ec
Alexander Malaev <am@malaev.net>
2017-08-19 12:26:19 +0300

    Add new/2 and set/3 helper methods for LWWRegister

--- lib/ecto_crdt_types/types/state/lwwregister.ex
+++ lib/ecto_crdt_types/types/state/lwwregister.ex
@@ -3,4 +3,18 @@ defmodule EctoCrdtTypes.Types.State.LWWRegister do
   @crdt_value_type :string
   @default_value ""
   use EctoCrdtTypes.Types.CRDT
+
+  def new(value \\ @default_value, timestamp_fn \\ &timestamp/0) do
+    crdt = @crdt_type.new()
+    set(crdt, value, timestamp_fn)
+  end
+
+  def set(crdt, value, timestamp_fn \\ &timestamp/0) do
+    {:ok, crdt} = @crdt_type.mutate({:set, timestamp_fn.(), value}, :unused, crdt)
+    crdt
+  end
+
+  defp timestamp do
+    DateTime.utc_now |> DateTime.to_unix(:microsecond)
+  end
 end
