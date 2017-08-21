3783950ff0a7830af9b74fdde715b55642901d1d
Alexander Malaev <am@malaev.net>
2017-08-22 00:47:07 +0300

    Add helpers for AWSet

--- lib/ecto_crdt_types/types/state/awset.ex
+++ lib/ecto_crdt_types/types/state/awset.ex
@@ -3,4 +3,28 @@ defmodule EctoCrdtTypes.Types.State.AWSet do
   @crdt_value_type {:array, :string}
   @default_value nil
   use EctoCrdtTypes.Types.CRDT
+
+  def new, do: @crdt_type.new()
+  def new(value, actor) when is_list(value), do: add_all(new(), value, actor)
+  def new(value, actor), do: add(new(), value, actor)
+
+  def add(crdt, value, actor) do
+    {:ok, crdt} = @crdt_type.mutate({:add, value}, actor, crdt)
+    crdt
+  end
+
+  def add_all(crdt, value, actor) do
+    {:ok, crdt} = @crdt_type.mutate({:add_all, value}, actor, crdt)
+    crdt
+  end
+
+  def rmv(crdt, value, actor) do
+    {:ok, crdt} = @crdt_type.mutate({:rmv, value}, actor, crdt)
+    crdt
+  end
+
+  def rmv_all(crdt, value, actor) do
+    {:ok, crdt} = @crdt_type.mutate({:rmv_all, value}, actor, crdt)
+    crdt
+  end
 end
---
+++ test/types/state/lwwregister_test.exs
@@ -0,0 +1,33 @@
+defmodule EctoCrdtTypes.Types.State.LWWRegisterTest do
+  use ExUnit.Case, async: true
+
+  alias EctoCrdtTypes.Types.State.LWWRegister
+
+  describe "#new/2" do
+    test "creates new lwwregister with default value" do
+      assert {:state_lwwregister, {_timestamp, ""}} = LWWRegister.new()
+    end
+
+    test "creates new lwwregister with passed value" do
+      assert {:state_lwwregister, {_timestamp, "value"}} = LWWRegister.new("value")
+    end
+
+    test "creates new lwwregister with custom timestamp function" do
+      timestamp_fun = fn -> 1234 end
+      assert {:state_lwwregister, {1234, "value"}} = LWWRegister.new("value", timestamp_fun)
+    end
+  end
+
+  describe "#set/3" do
+    test "sets new value to crdt" do
+      crdt = LWWRegister.new("old value")
+      assert {:state_lwwregister, {_timestamp, "new value"}} = LWWRegister.set(crdt, "new value")
+    end
+
+    test "sets new value using custom timestamp_fun" do
+      crdt = LWWRegister.new("old value", fn -> 1 end)
+      result = LWWRegister.set(crdt, "new value", fn -> 2 end)
+      assert {:state_lwwregister, {2, "new value"}} = result
+    end
+  end
+end
