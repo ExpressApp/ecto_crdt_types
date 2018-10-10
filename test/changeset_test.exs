fa7b562e1b2e7d8443b436d4d783490f78b4275b
Alexander Malaev <amalaev@ccsteam.ru>
2018-10-10 21:11:16 +0300

    Tests for atoms as keys in params

--- test/changeset_test.exs
+++ test/changeset_test.exs
@@ -44,6 +44,32 @@ defmodule EctoCrdtTypes.ChangesetTest do
     assert changeset.changes[:test] == ["a"]
   end
 
+  test "#cast_crdt/2 with atoms as keys in params merges crdt and sets value" do
+    crdt_to_merge = AWSet.crdt_type.new()
+    {:ok, crdt_to_merge} = AWSet.crdt_type.mutate({:add, "a"}, :a, crdt_to_merge)
+
+    expected_test = AWSet.crdt_type.merge(AWSet.crdt_type.new(), crdt_to_merge)
+
+    changeset = %Schema{} |> cast(%{test_crdt: crdt_to_merge}, [:name]) |> cast_crdt([:test])
+
+    assert changeset.changes[:test_crdt] == expected_test
+    assert changeset.changes[:test] == ["a"]
+  end
+
+  test "#cast_crdt/2 merges crdt and sets value for existing crdt field" do
+    crdt1 = AWSet.new(["a"], :a)
+    crdt1_1 = AWSet.add(crdt1, "c", :a)
+    crdt2 = AWSet.add(crdt1, "b", :b)
+
+    expected_test = AWSet.crdt_type.merge(crdt1_1, crdt2)
+
+    changeset = %Schema{test_crdt: crdt1_1, test: ["a", "c"]}
+                |> cast(%{test_crdt: crdt2}, [:name]) |> cast_crdt([:test])
+
+    assert changeset.changes[:test_crdt] == expected_test
+    assert Enum.sort(changeset.changes[:test]) == Enum.sort(["a", "b", "c"])
+  end
+
   test "#cast_crdt/2 with nil crdt field in schema merges crdt and sets value" do
     crdt_to_merge = AWSet.crdt_type.new()
     {:ok, crdt_to_merge} = AWSet.crdt_type.mutate({:add, "a"}, :a, crdt_to_merge)
