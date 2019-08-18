38195aad0d0ece65f85bedfaa85397552c1dfadb
Alexander Malaev <am@malaev.net>
2019-08-18 17:24:50 +0300

    Add EWFlag new/enable/disable APIs

--- lib/ecto_crdt_types/types/state/ewflag.ex
+++ lib/ecto_crdt_types/types/state/ewflag.ex
@@ -1,5 +1,19 @@
 defmodule EctoCrdtTypes.Types.State.EWFlag do
-  @crdt_type :state_dwflag
+  @crdt_type :state_ewflag
   @crdt_value_type :boolean
   use EctoCrdtTypes.Types.CRDT
+
+  def new, do: @crdt_type.new()
+  def new(true, actor), do: enable(new(), actor)
+  def new(false, actor), do: disable(new(), actor)
+
+  def enable(crdt, actor) do
+    {:ok, crdt} = @crdt_type.mutate(:enable, actor, crdt)
+    crdt
+  end
+
+  def disable(crdt, actor) do
+    {:ok, crdt} = @crdt_type.mutate(:disable, actor, crdt)
+    crdt
+  end
 end
