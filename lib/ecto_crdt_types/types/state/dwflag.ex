65087d11ddaa1ffbe0fffee73e5bbb21f74043a6
Alexander Malaev <amalaev@ccsteam.ru>
2019-10-10 11:16:19 +0300

    Add DWFlag and tests

--- lib/ecto_crdt_types/types/state/dwflag.ex
+++ lib/ecto_crdt_types/types/state/dwflag.ex
@@ -2,4 +2,24 @@ defmodule EctoCrdtTypes.Types.State.DWFlag do
   @crdt_type :state_dwflag
   @crdt_value_type :boolean
   use EctoCrdtTypes.Types.CRDT
+
+  def new, do: @crdt_type.new()
+  def new(true, actor), do: enable(new(), actor)
+  def new(false, actor), do: disable(new(), actor)
+
+  def enable(nil, actor) do
+    enable(new(), actor)
+  end
+  def enable(crdt, actor) do
+    {:ok, crdt} = @crdt_type.mutate(:enable, actor, crdt)
+    crdt
+  end
+
+  def disable(nil, actor) do
+    disable(new(), actor)
+  end
+  def disable(crdt, actor) do
+    {:ok, crdt} = @crdt_type.mutate(:disable, actor, crdt)
+    crdt
+  end
 end
